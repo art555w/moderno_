7470955d626ce65b96a87c0292a3204bd2fe99cc
Your Nameart555w <art555m@gmail.com>
2022-10-17 22:05:37 +0500

    adapted header and footer

--- app/js/app.js
+++ app/js/app.js
@@ -1,37 +1,46 @@
 $(function () {
-  $('.rate-star').rateYo({
-    rating: 5,
-    starWidth: '11px',
-    readOnly: true,
-  })
+   $('.rate-star').rateYo({
+      rating: 5,
+      starWidth: '11px',
+      readOnly: true,
+   })
 
-  $('.product-slider__inner').slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    dots: true,
-    arrows: false,
-  })
+   $('.product-slider__inner').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      dots: true,
+      arrows: false,
+   })
 
-  $('.js-range-slider').ionRangeSlider({
-    type: 'double',
-    grid: true,
-    min: 0,
-    max: 1000,
-    from: 0,
-    to: 600,
-    prefix: '$',
-  })
+   $('.js-range-slider').ionRangeSlider({
+      type: 'double',
+      grid: true,
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: '$',
+   })
 
-  $('.icon-list-ul').on('click', function () {
-    $('.product-page__content').addClass('list')
-    $('.icon-list-ul').addClass('active')
-    $('.icon-th-large').removeClass('active')
-  })
-  $('.icon-th-large').on('click', function () {
-    $('.product-page__content').removeClass('list')
-    $('.icon-th-large').addClass('active')
-    $('.icon-list-ul').removeClass('active')
-  })
+   $('.icon-th-list').on('click', function () {
+      $('.product-page__content').addClass('list')
+      $('.icon-th-list').addClass('active')
+      $('.icon-th-large').removeClass('active')
+   })
+   $('.icon-th-large').on('click', function () {
+      $('.product-page__content').removeClass('list')
+      $('.icon-th-large').addClass('active')
+      $('.icon-th-list').removeClass('active')
+   })
 
-  let mixer = mixitup('.products__inner')
+   $('.menu__btn').on('click', function () {
+      $('.menu__btn').toggleClass('active')
+      $('.menu__list').slideToggle()
+   })
+
+   $('.header__top-user').on('click', function () {
+      $('.header__register').toggleClass('active')
+   })
+
+   let mixer = mixitup('.products__inner')
 })
